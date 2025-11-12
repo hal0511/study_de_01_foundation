57f0d581e687824b34e372d6ac830f31f3901fc3
hal0511 <haaaaaruo0511@gmail.com>
2025-11-13 00:03:40 +0900

    [fix] .env

---
+++ db/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id SERIAL PRIMARY KEY
+    ,name TEXT NOT NULL
+    ,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (name)
+VALUES
+    ('Alis')
+    ,('Bob')
+    ,('Charlie')
+;
